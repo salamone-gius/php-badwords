09222fb968fdbbd96d1604f963e5c0a56c1df09f
salamone-gius <salamone.gius@gmail.com>
2022-06-23 14:40:45 +0200

    string and his length printed

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+    $nursery_rhyme = "Apelle, figlio di Apollo
+    fece una palla di pelle di pollo.
+    
+    Tutti i pesci
+    vennero a galla,
+    per vedere la palla
+    di pelle di pollo
+    fatta da Apelle,
+    figlio di Apollo.";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,6 +21,9 @@
         <title>PHP Badwords</title>
     </head>
     <body>
+        <h1>Apelle, figlio di Apollo</h1>
+        <p><?php echo $nursery_rhyme; ?></p>
+        <h4>Lunghezza: <?php echo mb_strlen($nursery_rhyme); ?></h4>
         
     </body>
 </html>
