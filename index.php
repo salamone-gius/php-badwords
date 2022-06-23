915bf1110580c2548623a6c64c557afe10a6a716
salamone-gius <salamone.gius@gmail.com>
2022-06-23 15:17:28 +0200

    try to censore

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
     di pelle di pollo
     fatta da Apelle,
     figlio di Apollo.";
+    
+    $censored_word = $_GET['censored'];
 
+    $censored_nursery_rhyme = str_replace($censored_word, "***", $nursery_rhyme);
 ?>
 
 
@@ -23,7 +26,10 @@
     <body>
         <h1>Apelle, figlio di Apollo</h1>
         <p><?php echo $nursery_rhyme; ?></p>
-        <h4>Lunghezza: <?php echo mb_strlen($nursery_rhyme); ?></h4>
-        
+        <h4>Lunghezza: <?php echo mb_strlen($nursery_rhyme); ?> caratteri.</h4>
+
+        <h1>Apelle, figlio di Apollo (censurata)</h1>
+        <p><?php echo $censored_nursery_rhyme; ?></p>
+        <h4>Lunghezza: <?php echo mb_strlen($censored_nursery_rhyme); ?> caratteri.</h4>
     </body>
 </html>
